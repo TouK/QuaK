a6e6ef3a4dca386385223c79123adafcb22b8d05
Agata Kłoss <akl@touk.pl>
2019-03-18 12:29:23 +0100

    more sfx

--- core/src/pl/touk/liero/system/SoundSystem.kt
+++ core/src/pl/touk/liero/system/SoundSystem.kt
@@ -5,7 +5,9 @@ import com.badlogic.gdx.audio.Sound
 
 class SoundSystem(enabled: Boolean) {
     enum class SoundSample {
-        Select, Back, Quack1, Quack2, Shoot1, Shoot2, Shoot3, Pew, Perish, Jump, Hurt, HurtLow
+        Quack1, Quack2, DuckExclaim, DuckHowl, DuckLong, DuckMed, DuckQuestion, DuckShort,
+        Select, Back, Shoot1, Shoot2, Shoot3, Pew, Perish, Jump, Hurt, HurtLow,
+        NesPew, NoAmmo, PewLong, PewLow,
     }
 
     private var enabled: Boolean = false
@@ -22,7 +24,17 @@ class SoundSystem(enabled: Boolean) {
             Pair(SoundSample.Perish, "sfx/sfx-perish.ogg"),
             Pair(SoundSample.Jump, "sfx/sfx-jump.ogg"),
             Pair(SoundSample.Hurt, "sfx/sfx-hurt.ogg"),
-            Pair(SoundSample.HurtLow, "sfx/sfx-hurt-low.ogg")
+            Pair(SoundSample.HurtLow, "sfx/sfx-hurt-low.ogg"),
+            Pair(SoundSample.DuckExclaim, "sfx/AV_duck_exclaim.ogg"),
+            Pair(SoundSample.DuckHowl, "sfx/AV_duck_howl.ogg"),
+            Pair(SoundSample.DuckLong, "sfx/AV_duck_long.ogg"),
+            Pair(SoundSample.DuckMed, "sfx/AV_duck_med.ogg"),
+            Pair(SoundSample.DuckQuestion, "sfx/AV_duck_question.ogg"),
+            Pair(SoundSample.DuckShort, "sfx/AV_duck_short.ogg"),
+            Pair(SoundSample.NoAmmo, "sfx/sfx-no-ammo.ogg"),
+            Pair(SoundSample.PewLong, "sfx/sfx-pew-long.ogg"),
+            Pair(SoundSample.PewLow, "sfx/sfx-pew-low.ogg"),
+            Pair(SoundSample.NesPew, "sfx/sfx-nes-pew.ogg")
     )
 
     init {
