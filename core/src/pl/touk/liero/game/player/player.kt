c256fd46be5a44d8f3473f94ca25e1782f82b59b
Mateusz Mazur <mmr@touk.pl>
2019-03-18 15:31:18 +0100

    Movement animation

--- core/src/pl/touk/liero/game/player/player.kt
+++ core/src/pl/touk/liero/game/player/player.kt
@@ -1,5 +1,6 @@
 package pl.touk.liero.game.player
 
+import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.physics.box2d.BodyDef
 import ktx.box2d.body
 import ktx.box2d.filter
@@ -12,6 +13,9 @@ import pl.touk.liero.game.cat_red
 import pl.touk.liero.game.joint.createWeaponJoint
 import pl.touk.liero.game.gun.Bazooka
 import pl.touk.liero.game.mask_red
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.utils.Array
+
 
 fun createPlayer(ctx: Ctx, x: Float, y: Float, playerControl: PlayerControl, weapon: Entity) {
     val playerBody = ctx.world.body(BodyDef.BodyType.DynamicBody) {
@@ -37,9 +41,41 @@ fun createPlayer(ctx: Ctx, x: Float, y: Float, playerControl: PlayerControl, wea
         texture(ctx.gameAtlas.findRegion("circle"), ctx.params.playerSize, ctx.params.playerSize)
         energy(ctx.params.playerTotalHealth)
         val bazooka = Bazooka(ctx)
-        script(PlayerScript(ctx, playerControl, bazooka))
+        val movementAnimation = createMovementAnimation(ctx)
+        val idleAnimation = createStandAnimation(ctx)
+        script(PlayerScript(ctx, playerControl, bazooka, movementAnimation, idleAnimation))
 
         // can be only one render script per Entity
         renderScript(HealthAndAmmoBar(ctx, bazooka))
     }
+}
+
+private fun createMovementAnimation(ctx: Ctx): Animation<TextureRegion> {
+    val walkFrames: Array<TextureRegion> = Array()
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation0"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation1"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation2"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation3"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation4"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation5"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation6"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation7"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation8"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation9"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation10"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation11"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation12"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation13"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobAnimation14"))
+    return Animation(0.025f, walkFrames, Animation.PlayMode.LOOP)
+}
+private fun createStandAnimation(ctx: Ctx): Animation<TextureRegion> {
+    val walkFrames: Array<TextureRegion> = Array()
+    walkFrames.add(ctx.gameAtlas.findRegion("blobIdle0"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobIdle1"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobIdle2"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobIdle3"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobIdle4"))
+    walkFrames.add(ctx.gameAtlas.findRegion("blobIdle5"))
+    return Animation(0.025f, walkFrames, Animation.PlayMode.LOOP)
 }
