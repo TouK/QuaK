ff5539530af47b96dabb6612f7d6081628dc915b
Rafal Golcz <rgl@touk.pl>
2019-03-20 14:59:40 +0100

    Przygotowanie jar'a

--- desktop/src/pl/touk/liero/desktop/LieroProd.kt
+++ desktop/src/pl/touk/liero/desktop/LieroProd.kt
@@ -5,13 +5,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import pl.touk.liero.Ctx
 import pl.touk.liero.GamePreferences
 import pl.touk.liero.LieroGame
-import pl.touk.liero.common.texturePacker
 import pl.touk.liero.gdx.DeferredApplication
 
-fun main(arg: Array<String>) {
-    texturePacker(arg, "../../graphics/game", "./", "game")
-    texturePacker(arg, "../../graphics/menu", "./", "menu")
-
+fun main() {
     val config = LwjglApplicationConfiguration()
     config.title = "Quak"
     LwjglApplication(DeferredApplication { LieroGame(Ctx(GamePreferences("liero"))) }, config)
